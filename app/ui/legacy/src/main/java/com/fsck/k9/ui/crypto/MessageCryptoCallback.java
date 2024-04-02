803fc9e5b769a79a6ad8b0b9b709874dbc47f774
cketti <cketti@thunderbird.net>
2024-04-02 15:14:23 +0200

    Add support for background activity start mode
    See https://developer.android.com/about/versions/14/behavior-changes-14#background-activity-restrictions

--- app/ui/legacy/src/main/java/com/fsck/k9/ui/crypto/MessageCryptoCallback.java
+++ app/ui/legacy/src/main/java/com/fsck/k9/ui/crypto/MessageCryptoCallback.java
@@ -10,6 +10,5 @@ import com.fsck.k9.mailstore.MessageCryptoAnnotations;
 public interface MessageCryptoCallback {
     void onCryptoHelperProgress(int current, int max);
     void onCryptoOperationsFinished(MessageCryptoAnnotations annotations);
-    boolean startPendingIntentForCryptoHelper(IntentSender si, int requestCode, Intent fillIntent,
-            int flagsMask, int flagValues, int extraFlags);
+    boolean startPendingIntentForCryptoHelper(IntentSender intentSender, int requestCode);
 }
